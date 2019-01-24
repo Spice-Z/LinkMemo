33a49fdd59fb181b3bf0bba91bfa8211d9821dcc
小倉優悟 <jigenbakdaaan@gmail.com>
2019-01-24 21:47:46 +0900

    update as product

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 window.onload = async function() {
-  // resetList();
   renderListFromStorage();
-
-  // console.log(list);
-
-  // list = setCurrentPage(list);
-  // storeList(list);
-  // renderMemoList();
 };
 
 function renderList(list) {
@@ -17,17 +10,38 @@ function renderList(list) {
   }
   memoList.innerHTML = "";
   list.forEach((el, index) => {
+    // ************createDOM like this *********
+    // <div id="1" class="listElement">
+    //   <a class="listElement-info" target="_blank" href="http://icooon-mono.com/">
+    //     <p class="listElemt-info-title">アイコン素材ダウンロードサイト「icooon-mono」 | 商用利用可能なアイコン素材が無料(フリー)ダウンロードできるサイト | 6000個以上のアイコン素材を無料でダウンロードできるサイト ICOOON MONO</p>
+    //     <p class="listElement-info-link">http://icooon-mono.com/</p>
+    //   </a>
+    //   <div class="listElement-remove"></div>
+    // </div>
+    const listElement = document.createElement("div");
+    const listElementInfo = document.createElement("a");
+    const listElementInfoTitle = document.createElement("p");
+    const listElementInfoLink = document.createElement("p");
+    const listElementRemove = document.createElement("div");
     const title = document.createElement("p");
     const link = document.createElement("a");
-    title.textContent = el.title;
-    title.setAttribute("id", index);
-    title.setAttribute("class", "listElement");
-    link.setAttribute("target", "_blank");
-    link.setAttribute("href", el.link);
-    link.textContent = ">";
-    title.appendChild(link);
-    memoList.appendChild(title);
-    const listItemNodes = document.getElementsByClassName("listElement");
+    listElement.setAttribute("class", "listElement");
+    listElementInfo.setAttribute("class", "listElement-info");
+    listElementInfo.setAttribute("target", "_blank");
+    listElementInfo.setAttribute("href", el.link);
+    listElementInfoTitle.textContent = el.title;
+    listElementInfoTitle.setAttribute("class", "listElement-info-title");
+    listElementInfoLink.textContent = el.link;
+    listElementInfoLink.setAttribute("class", "listElement-info-link");
+    listElementRemove.setAttribute("class", "listElement-remove");
+    listElementRemove.setAttribute("id", index);
+    listElementRemove.textContent = '☓';
+    listElementInfo.appendChild(listElementInfoTitle);
+    listElementInfo.appendChild(listElementInfoLink);
+    listElement.appendChild(listElementInfo);
+    listElement.appendChild(listElementRemove);
+    memoList.appendChild(listElement);
+    const listItemNodes = document.getElementsByClassName("listElement-remove");
     const listItems = Array.prototype.slice.call(listItemNodes);
     listItems.forEach(el => {
       el.addEventListener('click',function(e){
@@ -57,7 +71,7 @@ function addElementToStorage(addElement) {
     if (!Array.isArray(memoList)) {
       memoList = [];
     }
-    memoList.push(addElement);
+    memoList.unshift(addElement);
     chrome.storage.local.set({ memo: memoList }, () => {
       renderListFromStorage();
     });
@@ -82,13 +96,4 @@ function deleteItemFromList(num) {
     });
   });
 }
-
-// document.getElementById('addCurrentPage').addEventListener('click',()=>{
-//   addCurrentPage();
-//   console.log('current page is added');
-// })
-
-// document.getElementById('resetList').addEventListener('click',()=>{
-//   resetList();
-// })
 
--- js/listener.js
+++ js/listener.js
@@ -1,7 +1,6 @@
 
 document.getElementById('addCurrentPage').addEventListener('click',()=>{
   addCurrentPage();
-  console.log('current page is added');
 })
 
 document.getElementById('resetList').addEventListener('click',()=>{
@@ -11,20 +10,3 @@ document.getElementById('resetList').addEventListener('click',()=>{
 document.getElementById('resetList').addEventListener('click',()=>{
   resetList();
 })
-
-// const listItemNodes = document.getElementsByClassName('listElement');
-// const listItems = Array.prototype.slice.call(listItemNodes)
-// console.log('listitems');
-// console.log(listItems);
-
-// listItems.forEach((el,index) => {
-//   el.addEventListener('click'()=>{
-//     console.log('list Element is clicked');
-//     console.log(this);
-//   })
-// });
-
-// document.getElementsByClassName('listElement').addEventListener('click',()=>{
-//   console.log('list Element is clicked');
-//   console.log(this);
-// })

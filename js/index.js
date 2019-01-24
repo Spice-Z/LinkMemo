fd5c75658246a086b25dfd5608e30c68b1b4e40a
小倉優悟 <jigenbakdaaan@gmail.com>
2019-01-24 14:39:47 +0900

    add remove

--- js/index.js
+++ js/index.js
@@ -1,102 +1,94 @@
 window.onload = async function() {
   // resetList();
   renderListFromStorage();
+
   // console.log(list);
-  
+
   // list = setCurrentPage(list);
   // storeList(list);
   // renderMemoList();
 };
 
 function renderList(list) {
-    const memoList = document.getElementById("memoList");
-    console.log(list);
-    if (!Array.isArray(list) ) {
-      memoList.innerHTML = '';
-      return
-    }
-    console.log('render will ');
-    memoList.innerHTML = '';
-    list.forEach((el, index) => {
-      console.log(el);
-      const title = document.createElement("p");
-      const link = document.createElement("a");
-      title.textContent = el.title;
-      link.setAttribute("target", "_blank");
-      link.setAttribute("href", el.link);
-      link.setAttribute("id", index);
-      link.textContent = ">";
-      title.appendChild(link);
-      memoList.appendChild(title);
+  const memoList = document.getElementById("memoList");
+  if (!Array.isArray(list)) {
+    memoList.innerHTML = "";
+    return;
+  }
+  memoList.innerHTML = "";
+  list.forEach((el, index) => {
+    const title = document.createElement("p");
+    const link = document.createElement("a");
+    title.textContent = el.title;
+    title.setAttribute("id", index);
+    title.setAttribute("class", "listElement");
+    link.setAttribute("target", "_blank");
+    link.setAttribute("href", el.link);
+    link.textContent = ">";
+    title.appendChild(link);
+    memoList.appendChild(title);
+    const listItemNodes = document.getElementsByClassName("listElement");
+    const listItems = Array.prototype.slice.call(listItemNodes);
+    listItems.forEach(el => {
+      el.addEventListener('click',function(e){
+        deleteItemFromList(this.id)
+      })
     });
+  });
 }
 
 function addCurrentPage() {
-    chrome.tabs.getSelected(null, (tab) => {
+  chrome.tabs.getSelected(null, tab => {
     const title = tab.title;
     const url = tab.url;
-    console.log('current info is');
-    console.log(title);
-    console.log(url);
-    addElementToStorage({'title':title,'link':url});
+    addElementToStorage({ title: title, link: url });
   });
 }
 
 function resetList() {
-  chrome.storage.local.remove('memo',result => {
-    console.log(result);
-    console.log('remove is done');
+  chrome.storage.local.remove("memo", result => {
     renderList();
-  })
+  });
 }
 
 function addElementToStorage(addElement) {
-  console.log('addelement');
-  console.log(addElement);
   chrome.storage.local.get(["memo"], result => {
-    console.log(memoList);
     memoList = result.memo;
-    if(!Array.isArray(memoList)){
+    if (!Array.isArray(memoList)) {
       memoList = [];
     }
     memoList.push(addElement);
-    console.log('newlist');
-    console.log(memoList);
     chrome.storage.local.set({ memo: memoList }, () => {
-      console.log("Value is set to " + memoList);
       renderListFromStorage();
     });
-  })
-}
-
-function updateList(addElement) {
-  // chrome.storage.local.set({ memo: memoList }, () => {
-  //   console.log("Value is set to " + sample);
-  // });
-  chrome.storage.local.get(["memo"], result => {
-    memoList = result.memo;
-    return memoList
-  }).then(memoList =>{
-    memoList.push(addElement)
-    chrome.storage.local.set({ memo: memoList }, () => {
-      console.log("Value is set to " + memoLtis);
-    });
-  })
+  });
 }
 
 function renderListFromStorage() {
   chrome.storage.local.get(["memo"], result => {
-    console.log('取得データ↓');
-    console.log(result.memo);
-    renderList(result.memo)
-  })
+    renderList(result.memo);
+  });
 }
 
-document.getElementById('addCurrentPage').addEventListener('click',()=>{
-  addCurrentPage();
-  console.log('current page is added');
-})
+function deleteItemFromList(num) {
+  chrome.storage.local.get(["memo"], result => {
+    memoList = result.memo;
+    if (!Array.isArray(memoList)) {
+      memoList = [];
+    }
+    memoList.splice(num, 1);
+    chrome.storage.local.set({ memo: memoList }, () => {
+      renderListFromStorage();
+    });
+  });
+}
 
-document.getElementById('resetList').addEventListener('click',()=>{
-  resetList();
-})
+// document.getElementById('addCurrentPage').addEventListener('click',()=>{
+//   addCurrentPage();
+//   console.log('current page is added');
+// })
+
+// document.getElementById('resetList').addEventListener('click',()=>{
+//   resetList();
+// })
+
---
+++ js/listener.js
@@ -0,0 +1,30 @@
+
+document.getElementById('addCurrentPage').addEventListener('click',()=>{
+  addCurrentPage();
+  console.log('current page is added');
+})
+
+document.getElementById('resetList').addEventListener('click',()=>{
+  resetList();
+})
+
+document.getElementById('resetList').addEventListener('click',()=>{
+  resetList();
+})
+
+// const listItemNodes = document.getElementsByClassName('listElement');
+// const listItems = Array.prototype.slice.call(listItemNodes)
+// console.log('listitems');
+// console.log(listItems);
+
+// listItems.forEach((el,index) => {
+//   el.addEventListener('click'()=>{
+//     console.log('list Element is clicked');
+//     console.log(this);
+//   })
+// });
+
+// document.getElementsByClassName('listElement').addEventListener('click',()=>{
+//   console.log('list Element is clicked');
+//   console.log(this);
+// })
